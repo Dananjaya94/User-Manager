a881a5d3289bfda8804a2bac9c99c68b043c3b15
Dananjaya Polwattage <dananjayapolwattage@gmail.com>
2023-07-18 09:31:04 +0530

    UserList Component
    Bug fixes

--- src/components/UserList/UserList.js
+++ src/components/UserList/UserList.js
@@ -5,34 +5,14 @@ import classes from "./UserList.module.css";
 
 export default function UserList(props) {
   return (
-    // <table>
-    //   <thead>
-    //     <tr>
-    //       <td>UserName</td>
-    //       <td>Age</td>
-    //     </tr>
-    //   </thead>
-    //   <tbody>
-    //     {props.data.map((user) => (
-    //       <tr>
-    //         <td>{user.userName}</td>
-    //         <td>{user.age}</td>
-    //       </tr>
-    //     ))}
-    //   </tbody>
-    // </table>
-
-    <ul>
-      {props.data.map((user) => (
-        <li>
-          <Card className={classes.list}>
-            <div className={classes.users}>
-              <h2>{user.userName}</h2>
-              <div className={classes.userage}>{user.age}</div>
-            </div>
-          </Card>
-        </li>
-      ))}
-    </ul>
+    <Card className={classes.users}>
+      <ul>
+        {props.data.map((user) => (
+          <li>
+            {user.userName} ({user.age} years old)
+          </li>
+        ))}
+      </ul>
+    </Card>
   );
 }
