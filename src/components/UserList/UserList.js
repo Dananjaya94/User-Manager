248047b3fa68890bf0ad11f95a9dfa2455d9c7dc
Dananjaya Polwattage <dananjayapolwattage@gmail.com>
2023-07-18 08:15:27 +0530

    UserList
    UserList component added with the CSS module.

--- src/components/UserList/UserList.js
+++ src/components/UserList/UserList.js
@@ -1,5 +1,38 @@
 import React from "react";
+import Card from "../UI/Card";
 
-export default function UserList() {
-  return <div>UserList</div>;
+import classes from "./UserList.module.css";
+
+export default function UserList(props) {
+  return (
+    // <table>
+    //   <thead>
+    //     <tr>
+    //       <td>UserName</td>
+    //       <td>Age</td>
+    //     </tr>
+    //   </thead>
+    //   <tbody>
+    //     {props.data.map((user) => (
+    //       <tr>
+    //         <td>{user.userName}</td>
+    //         <td>{user.age}</td>
+    //       </tr>
+    //     ))}
+    //   </tbody>
+    // </table>
+
+    <ul>
+      {props.data.map((user) => (
+        <li>
+          <Card className={classes.list}>
+            <div className={classes.users}>
+              <h2>{user.userName}</h2>
+              <div className={classes.userage}>{user.age}</div>
+            </div>
+          </Card>
+        </li>
+      ))}
+    </ul>
+  );
 }
